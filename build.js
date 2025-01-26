acecd842c76dfd9e9ea7dc422c7d6dde356c2fdd
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-01-26 14:54:55 +0800

    update

--- build.js
+++ build.js
@@ -8,6 +8,13 @@ esbuild
     platform: 'neutral', // Cloudflare Workers use a specific runtime, so avoid `node` platform features
     format: 'esm', // Use ECMAScript modules
     target: 'es2022', // Cloudflare Workers support modern JavaScript
-    minify: true // Minify the output for better performance
+    minify: true, // Minify the output for better performance,
+    charset: 'utf8',
+    sourcemap: false,
+    outfile: '<injected>',
+    minify: true, // disable via `--no-minify` flag
+    resolveExtensions: ['.tsx', '.ts', '.jsx', '.mjs', '.js', '.json'],
+    mainFields: ['worker', 'browser', 'module', 'jsnext', 'main'],
+    conditions: ['worker', 'browser', 'import', 'production']
   })
   .catch(() => process.exit(1));
