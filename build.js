e15ea4e0e2551f9d95a3f0252a341dd4159a473a
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-01-26 14:56:19 +0800

    update

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ esbuild
     entryPoints: ['./src/index.ts'], // Entry point of your Cloudflare Worker code
     bundle: true, // Bundle all dependencies into one file
     outfile: './dist/worker.js', // Output file
-    platform: 'neutral', // Cloudflare Workers use a specific runtime, so avoid `node` platform features
+    platform: 'node',
     format: 'esm', // Use ECMAScript modules
     target: 'es2022', // Cloudflare Workers support modern JavaScript
     minify: true, // Minify the output for better performance,
