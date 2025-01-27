d818bdc951b66f977b156f2f0c759b394534236d
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-01-27 17:47:06 +0800

    update

--- src/send.ts
+++ src/send.ts
@@ -7,8 +7,7 @@ export async function send(event, env, ctx) {
   // Retrieve scheduled tasks
   const schedules = await listSchedules(now, env);
   for (const schedule of schedules) {
-    const time = (schedule.TimeStamp + schedule.EstimateTime * 1000 - now) / 1000;
-    const message = `公車將在${formatTime(time, schedule.TimeFormattingMode)}內抵達${schedule.LocationName} | 路線：${schedule.RouteName} - 往${schedule.Direction}`;
+    const message = `公車將在${formatTime(schedule.TimeOffset * -60, schedule.TimeFormattingMode)}內抵達${schedule.LocationName} | 路線：${schedule.RouteName} - 往${schedule.Direction}`;
     const url = new URL('https://erichsia7.github.io/bus/');
     url.searchParams.set('permalink', `0@${parseInt(schedule.RouteID).toString(16)}~${schedule.RouteName}`);
     await sendTextMessageViaTelegram(env.TELEGRAM_BOT_TOKEN, env.TELEGRAM_CHAT_ID, message, url.toString());
