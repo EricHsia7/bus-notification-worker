fc94eb1f5f59d9ec25cfb90e0219d912a7ba24a6
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-20 18:05:50 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,7 @@ export default {
     const param_method = url_params.get('method');
 
     const contentType = request.headers.get('content-type');
+    console.log(contentType);
     if (String(contentType).includes('application/json')) {
       const requestBody = await request.json();
       switch (param_method) {
